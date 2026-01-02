6a9b956dbfa794fd3a09fed96dc9400a874c80d5
John Rothen <rackjothen@gmail.com>
2026-01-02 14:06:50 -0500

    Figured out the default package documentation

---
+++ R/fssg-package.R
@@ -0,0 +1,9 @@
+#' fssg
+#'
+#' The \pkg{fssg} package provides a simple format for the bulk testing of parametric survival distributions. Simply provide your model, and let the package tell you what distribution fits best.
+#' @details
+#' More stuff
+#' @author John Rothen, \email{jmrothen.business@gmail.com}
+#' @docType package
+#' @name fssg_package
+"_PACKAGE"
